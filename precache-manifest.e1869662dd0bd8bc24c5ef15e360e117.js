30d18025d7ffe6cc17c408ec9995da431e20020b
Lanka-92791 <92791@fhss.sjp.ac.lk>
2021-06-11 21:43:18 +0700

    Updates

---
+++ precache-manifest.e1869662dd0bd8bc24c5ef15e360e117.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce09bdf61916c6d1322a1dc17920c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be534eab105cf7d94472",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "3b2c48525b21fca07346",
+    "url": "/static/js/2.0b538cd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0b538cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be534eab105cf7d94472",
+    "url": "/static/js/main.70120157.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "d67bca1278a4d6ced2dae26e9c3cea9e",
+    "url": "/static/media/CV.d67bca12.pdf"
+  },
+  {
+    "revision": "c93a8ec6fdd5a00cae98417e4dccf239",
+    "url": "/static/media/Lanka.c93a8ec6.jpg"
+  }
+]);
